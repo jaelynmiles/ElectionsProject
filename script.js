5b509c2827e8af4c40696bfdf1e239c1435d6d91
jaelynmiles <jaelynmiles@gmail.com>
2018-06-10 11:47:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-"Hello World" 
+$(window).scroll(function(e){
+  parallax();
+});
+
+function parallax(){
+  var scrolled = $(window).scrollTop();
+  $('.background').css('top',-(scrolled*0.15)+'px');
+}
+var buttons = document.querySelectorAll('button');
+
+// For each button on the page, call createParagraph when clicked.
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', createParagraph);
+}
+
